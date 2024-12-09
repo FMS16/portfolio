762e881e7bd970782f7cde64c90be469052260f2
Fabricio <martinsecondo16@gmail.com>
2024-12-09 12:29:40 -0300

    changes link footer

--- app/works/page.tsx
+++ app/works/page.tsx
@@ -15,11 +15,11 @@ const page = () => {
             <div className="lg-w-[45%] w-full px-12 md:px-0 md:justify-center md:flex-row flex items-start flex-col">
               <ul className="text-lg dark:text-[#8798c9]">
                 <li className="text-xl mb-4 text-[#8798c9] tracking-widest sf-bold dark:text-[#8798c9]">Elsewhere</li>
-                <li className="my-2 opacity-50 hover:opacity-100"><a href="">Linkedin</a></li>
-                <li className="my-2 opacity-50 hover:opacity-100"><a href="">Github</a></li>
-                <li className="my-2 opacity-50 hover:opacity-100"><a href="">Contact</a></li>
-                <li className="my-2 opacity-50 hover:opacity-100"><a href="">Posts</a></li>
-                <li className="my-2 opacity-50 hover:opacity-100"><a href="">Youtube</a></li>
+                <li className="my-2 opacity-50 hover:opacity-100"><a href="https://www.linkedin.com/in/fabricio-secondo-a22262231/">Linkedin</a></li>
+                <li className="my-2 opacity-50 hover:opacity-100"><a href="https://github.com/FMS16?tab=repositories">Github</a></li>
+                <li className="my-2 opacity-50 hover:opacity-100"><a href="mailto:martinsecondo16@gmail.com">Contact</a></li>
+                <li className="my-2 opacity-50 hover:opacity-100"><a href="https://www.linkedin.com/in/fabricio-secondo-a22262231/">Posts</a></li>
+                <li className="my-2 opacity-50 hover:opacity-100"><a href="https://www.youtube.com/@elrincondealanturing">Youtube</a></li>
               </ul>
             </div>
             <div className="w-[100%] lg:w-full mt-32 md:mt-0 flex flex-wrap justify-center">
